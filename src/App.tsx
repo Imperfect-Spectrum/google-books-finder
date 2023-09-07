97d52b15e21321f54d8e18177970dd564e4889c5
Akamee <nikitasorokin110901@gmail.com>
2023-09-08 00:53:12 +0300

    fix-bugs-and-add-style-to-booksResult

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
 import { BooksResult } from "./components/booksResult";
 import { SearchHeader } from "./components/headerSearch";
-import { useAppSelector } from "./hook";
-import { RootState } from "./store";
 
 function App() {
-  const books = useAppSelector((state: RootState) => state.books);
-  console.log(books[0].items[0]);
   return (
     <div className="w-[100%] mr-auto ml-auto lg:w-[60%]">
       <SearchHeader />
--- src/components/booksResult/index.tsx
+++ src/components/booksResult/index.tsx
@@ -3,18 +3,37 @@ import { RootState } from "../../store";
 
 export function BooksResult() {
   const books = useAppSelector((state: RootState) => state.books);
+  if (!books || books.length === 0) {
+    return <></>;
+  }
+
   return (
-    <div className="flex flex-col w-[100%] bg-slate-300 py-6">
+    <div className="flex flex-col w-[100%] py-6">
       <div className="flex flex-col sm:w-[50%] mx-auto">
-        <h1 className="mb-4  leading-none tracking-tight text-center text-gray-900 text-1xl md:text-2xl">
+        <h1 className="mb-4 leading-none tracking-tight text-center text-gray-900 text-1xl md:text-2xl">
           Found {books[0].totalItems} result
         </h1>
       </div>
       <div className="flex flex-wrap justify-center items-center gap-5 mx-auto">
         {books[0].items.map((book) => (
-          <div className="flex  flex-col justify-center items-center bg-slate-500 w-[30%] h-[300px]">
-            <img className="" src={book.volumeInfo.imageLinks.smallThumbnail} alt={book.volumeInfo.title}/>
-            <p className="underline text-gray-900">{book.volumeInfo.categories}</p>
+          <div
+            className="flex flex-col  items-center bg-[rgb(235,235,235)] w-[30%] min-h-[330px]"
+            key={book.id}
+          >
+            <img
+              className="w-[128px] h-[182]"
+              src={book.volumeInfo.imageLinks?.smallThumbnail || ""}
+              alt={book.volumeInfo.title}
+            />
+            <p className="underline text-gray-900 mr-auto ml-4">
+              {book.volumeInfo.categories}
+            </p>
+            <p className="text-gray-900 font-extrabold mr-auto ml-4">
+              {book.volumeInfo.title}
+            </p>
+            <p className=" text-gray-900 mr-auto ml-4">
+              {book.volumeInfo.authors?.join(", ")}
+            </p>
           </div>
         ))}
       </div>
